9f28b6fa0cfc991a7eb57ec77b8db5d8681ba499
ViswaRamanthan <rviswa0047@gmail.com>
2025-04-16 15:17:22 +0530

    First commit | Initated the project

---
+++ src/test/java/com/git/git_demo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.git_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
